84b156b694b8c5103e08189dc69de2953c2b36ae
Long Nguyen <ivoglent@Longs-MacBook-Pro.local>
2019-05-03 11:14:03 +0700

    Add minify processor

---
+++ Module.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivoglent
+ * Date: 5/3/19
+ * Time: 08:52
+ */
+
+namespace ivoglent\yii2\minify;
+
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+use yii\base\Event;
+use yii\base\View;
+
+class Module extends \yii\base\Module implements BootstrapInterface
+{
+    public $configs = [];
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        $app->setComponents([
+            'minifier' => [
+                'class' => Minifier::className(),
+                'configs' => $this->configs
+            ]
+        ]);
+        if ($app instanceof \yii\web\Application) {
+            $app->view->on(View::EVENT_END_PAGE, [$app->minifier, 'processView']);
+            //Html compressing
+            $app->response->on(\yii\web\Response::EVENT_BEFORE_SEND, [$app->minifier, 'processResponse']);
+        }
+        // TODO: Implement bootstrap() method.
+    }
+}
